c5a0a1f6ea0700b8e79e1ac6588a2d8f63de2985
Sergio Filipe Fidalgo Pereira de Carvalho <sergioffpc@gmail.com>
2021-12-11 22:42:19 +0000

    Refactor main renderer.

--- lib/obscura.ex
+++ lib/obscura.ex
@@ -1,63 +1,110 @@
 defmodule Obscura do
-  alias Obscura.{Bounds2, Point2}
+  defstruct [:broker, :scene, :step]
 
-  def render(scene, options \\ []) do
-    broker = Keyword.get(options, :broker, "amqp://guest:guest@localhost")
-    step = Keyword.get(options, :step, 4)
+  alias Obscura.{Consumer, Dispatcher, Scene}
 
+  def new(uri, options \\ []) do
+    scene = Scene.parse!(uri)
+
+    %__MODULE__{
+      broker: Keyword.get(options, :broker, "amqp://guest:guest@localhost"),
+      scene: scene,
+      step: Keyword.get(options, :step, 256)
+    }
+  end
+
+  def dispatch(renderer) do
+    Dispatcher.dispatch(renderer.broker, renderer)
+  end
+
+  def consume(renderer) do
+    Consumer.consume(renderer.broker, renderer)
+  end
+end
+
+defprotocol Obscura.Renderer do
+  def aggregate(renderer, pixels)
+
+  def integrate(renderer, tile)
+
+  def shred(renderer)
+end
+
+defimpl Obscura.Renderer, for: Obscura do
+  def aggregate(renderer, pixels) do
+    Enum.each(pixels, fn [xy, rgb] -> put_pixel(renderer, xy, rgb) end)
+  end
+
+  def integrate(_renderer, [min_x, min_y, max_x, max_y]) do
+    for y <- Range.new(min_y, max_y - 1),
+        x <- Range.new(min_x, max_x - 1),
+        into: [],
+        do: [{x, y}, {0, 0, 0}]
+  end
+
+  def shred(renderer) do
+    resolution = renderer.scene.camera.film.resolution
+    step = renderer.step
+
+    for y <- Range.new(0, resolution.y - 1, step),
+        x <- Range.new(0, resolution.x - 1, step),
+        into: [] do
+      [x, y, x + step, y + step]
+    end
+  end
+
+  defp put_pixel(_renderer, xy, rgb) do
+    IO.puts("xy: #{xy}, rgb: #{rgb}")
+  end
+end
+
+defmodule Obscura.Dispatcher do
+  def dispatch(broker, renderer) do
     {:ok, connection} = AMQP.Connection.open(broker)
     {:ok, channel} = AMQP.Channel.open(connection)
 
     {:ok, %{message_count: 0}} = AMQP.Queue.declare(channel, "obscura.rendering.queue")
+    {:ok, %{queue: reply_to}} = AMQP.Queue.declare(channel)
 
-    {:ok, %{queue: reply_to}} = AMQP.Queue.declare(channel, "", exclusive: true)
-    AMQP.Basic.consume(channel, reply_to, self(), no_ack: true)
+    for bounds <- renderer.shred(renderer) do
+      Task.async(fn ->
+        AMQP.Basic.consume(channel, reply_to, self(), no_ack: true)
 
-    bounds = Bounds2.new(Point2.new(), Point2.new(16, 16))
+        pixels =
+          JSON.encode!(bounds)
+          |> basic_publish(channel, reply_to)
+          |> basic_deliver()
+          |> JSON.decode!()
 
-    for tile <- tiles(bounds, step) do
-      json = JSON.encode!(scene: scene, bounds: {tile.min.x, tile.min.y, tile.max.x, tile.max.y})
-      Task.async(fn -> basic_publish(json, channel, reply_to) end)
+        renderer.aggregate(renderer, pixels)
+      end)
     end
     |> Enum.map(&Task.await(&1, :infinity))
-    |> Enum.map(&basic_deliver/1)
+
+    AMQP.Connection.close(connection)
   end
 
   defp basic_deliver(correlation_id) do
     receive do
       {:basic_deliver, payload, %{correlation_id: ^correlation_id}} ->
-        JSON.decode!(payload)
+        payload
     end
   end
 
-  defp basic_publish(json, channel, reply_to) do
+  defp basic_publish(payload, channel, reply_to) do
     correlation_id = :erlang.unique_integer() |> :erlang.integer_to_binary() |> Base.encode64()
 
-    AMQP.Basic.publish(channel, "", "obscura.rendering.queue", json,
-      reply_to: reply_to,
-      correlation_id: correlation_id
+    AMQP.Basic.publish(channel, "", "obscura.rendering.queue", payload,
+      correlation_id: correlation_id,
+      reply_to: reply_to
     )
 
     correlation_id
   end
-
-  defp tiles(bounds, step) do
-    for y <- Range.new(bounds.min.y, bounds.max.y - 1, step),
-        x <- Range.new(bounds.min.x, bounds.max.x - 1, step),
-        into: [] do
-      min = Point2.new(x, y)
-      max = Point2.new(x + step, y + step)
-      Bounds2.new(min, max)
-    end
-  end
 end
 
-defmodule Obscura.Worker do
-  alias Obscura.{Point2, Scene}
-
-  def start(options \\ []) do
-    broker = Keyword.get(options, :broker, "amqp://guest:guest@localhost")
-
+defmodule Obscura.Consumer do
+  def consume(broker, renderer) do
     {:ok, connection} = AMQP.Connection.open(broker)
     {:ok, channel} = AMQP.Channel.open(connection)
 
@@ -65,13 +112,15 @@ defmodule Obscura.Worker do
     AMQP.Basic.qos(channel, prefetch_count: 1)
     AMQP.Basic.consume(channel, "obscura.rendering.queue")
 
-    basic_deliver(channel)
+    basic_deliver(channel, renderer)
+
+    AMQP.Connection.close(connection)
   end
 
-  defp basic_deliver(channel) do
+  defp basic_deliver(channel, renderer) do
     receive do
       {:basic_deliver, payload, metadata} ->
-        json = JSON.decode!(payload) |> handle |> JSON.encode!()
+        json = renderer.integrate(renderer, JSON.decode!(payload)) |> JSON.encode!()
 
         AMQP.Basic.publish(channel, "", metadata.reply_to, "#{json}",
           correlation_id: metadata.correlation_id
@@ -79,23 +128,7 @@ defmodule Obscura.Worker do
 
         AMQP.Basic.ack(channel, metadata.delivery_tag)
 
-        basic_deliver(channel)
+        basic_deliver(channel, renderer)
     end
-  end
-
-  defp handle(request) do
-    _scene = Scene.parse!(request["scene"])
-
-    for pixel <- pixels(request["bounds"]) do
-      Task.async(fn -> [xy: {pixel.x, pixel.y}, rgb: {0, 0, 0}] end)
-    end
-    |> Enum.map(&Task.await/1)
-  end
-
-  defp pixels([min_x, min_y, max_x, max_y] = _tile) do
-    for y <- Range.new(min_y, max_y - 1),
-        x <- Range.new(min_x, max_x - 1),
-        into: [],
-        do: Point2.new(x, y)
   end
 end
